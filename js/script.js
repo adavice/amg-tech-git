96aa9e3f6f637f95b37edf1b3308051a09c22827
adavice <97439259+adavice@users.noreply.github.com>
2024-07-04 11:17:05 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  /*$(document).on('click', 'a[href^="#"]', function (event) {
+      event.preventDefault();
+
+      $('html, body').animate({
+          scrollTop: $($.attr(this, 'href')).offset().top
+      }, 500);
+  });*/
+
+  $(function () {
+    $('#services').hover(function () {
+      $(this).find('ul').first().stop().slideDown(200);
+    }, function () {
+      $(this).find('ul').stop().slideUp(50);
+    });
+  });
+});
+
+let today = new Date();
+let year = today.getFullYear();
+$('#year').text(year);
